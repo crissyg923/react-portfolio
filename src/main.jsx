3b52c3bf94a6e16e082542510aead24a9aeafbbb
Crystal Green <crissybrown1992@icloud.com>
2023-11-24 15:31:07 -0500

    added pages, set up proper react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
+import App from './App';
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
+import AboutPage from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage'
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
+
