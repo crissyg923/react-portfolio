e8613d8f5c22776b16b24749427ed4d909f87a37
Crystal Green <crissybrown1992@icloud.com>
2023-12-05 22:15:34 -0500

    removed fixed from footer

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,66 +1,11 @@
-import { Link, useLocation } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 
 function NavTabs() {
-  const currentPage = useLocation().pathname;
 
   return (
   <header>
-    {/* <div className="container-fluid navdisplay">
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <div>
-       <a className="navbar-brand cg" href="/">CG</a>
-       </div>
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link
-          to="/"
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Resume"
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Aboutme' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Portfolio"
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-        >
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Contact"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          to="/Aboutme"
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Aboutme' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </Link>
-      </li>
-    </ul>
-   
-    </nav>
-    </div> */}
+
     <Navbar className="header"  expand="lg">
   <Navbar.Brand className="cg" href="#home">CG</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -78,7 +23,7 @@ function NavTabs() {
       <LinkContainer to="/resume">
       <Nav.Link href="/resume">Resume</Nav.Link>
       </LinkContainer>
-      <LinkContainer to="/contactme">
+      <LinkContainer to="/contact">
       <Nav.Link href="/contact">Contact Me</Nav.Link>
       </LinkContainer>
     </Nav>
